9f86408a757afb589a3c317a9ba60af6a259d574
tfsn <tfsn86@gmail.com>
2020-02-11 22:53:25 +0100

    Testing data fetching from API

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './styles/global.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	state = { courses: [] };
+
+	componentDidMount() {
+		fetch('http://localhost:5000/api/courses')
+			.then(res => res.json())
+			.then(data => this.setState({ courses: data }))
+			.catch(console.log);
+	}
+
+	render() {
+		return <li>TBA</li>;
+	}
 }
 
 export default App;
