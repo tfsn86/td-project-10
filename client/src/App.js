d1d52db8850ad6adfb4fa6acc1b5f6b1ba142b57
tfsn <tfsn86@gmail.com>
2020-02-16 15:58:26 +0100

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,10 @@
 import React, { Component } from 'react';
+import Courses from './components/Courses';
 import './styles/global.css';
 
 class App extends Component {
-	state = { courses: [] };
-
-	componentDidMount() {
-		fetch('http://localhost:5000/api/courses')
-			.then(res => res.json())
-			.then(data => this.setState({ courses: data }))
-			.catch(console.log);
-	}
-
 	render() {
-		return <li>TBA</li>;
+		return <Courses />;
 	}
 }
 
