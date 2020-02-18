ccf6908d7a86012865bf37f1cfe0785d45cc5da2
tfsn <tfsn86@gmail.com>
2020-02-18 19:52:02 +0100

    Initial commit and JSX added

---
+++ client/src/components/NotFound.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class NotFound extends Component {
+	render() {
+		<div className="bounds">
+			<h1>Not Found</h1>
+			<p>Sorry! We couldn't find the page you're looking for.</p>
+		</div>;
+	}
+}
+
+export default NotFound;
