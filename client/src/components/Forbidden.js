8599d71f89b08be2948dd3e74f86e388a0e7216d
tfsn <tfsn86@gmail.com>
2020-02-18 20:01:10 +0100

    Initial commit and JSX added

---
+++ client/src/components/Forbidden.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Forbidden extends Component {
+	render() {
+		return (
+			<div className="bounds">
+				<h1>Forbidden</h1>
+				<p>Oh oh! You can't access this page.</p>
+			</div>
+		);
+	}
+}
+
+export default Forbidden;
