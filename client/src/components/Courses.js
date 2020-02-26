89c1cff6035cb2aca6c213c58d933e2cd5dbd7b6
tfsn <tfsn86@gmail.com>
2020-02-26 07:08:42 +0100

    Navlinks and state management

--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 
 class Courses extends Component {
 	state = { courses: [] };
@@ -14,37 +15,40 @@ class Courses extends Component {
 		const { courses } = this.state;
 
 		return (
-			<div>
-				<div className="bounds">
-					<div className="grid-33">
-						<a
-							className="course--module course--link"
-							href="course-detail.html"
-						>
-							<h4 className="course--label">Course</h4>
-							<h3 className="course--title">Build a Basic Bookcase</h3>
-						</a>
-					</div>
-					<div className="grid-33">
-						<a
-							className="course--module course--add--module"
-							href="create-course.html"
-						>
-							<h3 className="course--add--title">
-								<svg
-									version="1.1"
-									xmlns="http://www.w3.org/2000/svg"
-									x="0px"
-									y="0px"
-									viewBox="0 0 13 13"
-									className="add"
-								>
-									<polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 " />
-								</svg>
-								New Course
-							</h3>
-						</a>
-					</div>
+			<div className="bounds">
+				{courses.map(course => {
+					return (
+						<div className="grid-33" key={`${course.id}`}>
+							<NavLink
+								to={`/courses/${course.id}`}
+								className="course--module course--link"
+							>
+								<h4 className="course--label">Course</h4>
+								<h3 className="course--title">{`${course.title}`}</h3>
+							</NavLink>
+						</div>
+					);
+				})}
+
+				<div className="grid-33">
+					<NavLink
+						to="/courses/create"
+						className="course--module course--add--module"
+					>
+						<h3 className="course--add--title">
+							<svg
+								version="1.1"
+								xmlns="http://www.w3.org/2000/svg"
+								x="0px"
+								y="0px"
+								viewBox="0 0 13 13"
+								className="add"
+							>
+								<polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon>
+							</svg>
+							Create Course
+						</h3>
+					</NavLink>
 				</div>
 			</div>
 		);
@@ -52,11 +56,3 @@ class Courses extends Component {
 }
 
 export default Courses;
-
-// return (
-//   <div>
-//     {courses.map(course => {
-//       return <li>{course.title}</li>;
-//     })}
-//   </div>
-// );
